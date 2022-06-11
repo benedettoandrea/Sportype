a783a78a8f553eeffb562560646146c80972f6f2
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-06-12 00:18:17 +0200

    avanzamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector(".navbar");
 const navbarSupportedContent = document.getElementById(
-  "navbarSupportedContent"
+  "navbar-supported-content"
 );
 navbarSupportedContent.addEventListener("shown.bs.collapse", function () {
   navbar.classList.add("navbar-shadow");
@@ -10,23 +10,45 @@ navbarSupportedContent.addEventListener("hidden.bs.collapse", function () {
     navbar.classList.remove("navbar-shadow");
   }
 });
+
 window.onscroll = () => {
   if (window.scrollY > 0) {
     navbar.classList.add("navbar-shadow");
   } else if (window.scrollY == 0) {
     navbar.classList.remove("navbar-shadow");
   }
-  if (window.scrollY > document.getElementById("slide-1").offsetHeight) {
-    navbar.classList.add("navbar-light");
-    navbar.classList.remove("navbar-dark");
-    document
-      .querySelector("meta[name=theme-color]")
-      .setAttribute("content", "#f1f1f1");
+
+  if (document.getElementById("home-slide-1") != null) {
+    // home page
+    if (window.scrollY > document.getElementById("home-slide-1").offsetHeight) {
+      navbar.classList.add("navbar-light", "bgneutral");
+      navbar.classList.remove("navbar-dark");
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", "#f1f1f1");
+    } else {
+      navbar.classList.add("navbar-dark");
+      navbar.classList.remove("navbar-light", "bgneutral");
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", "#7885FF");
+    }
   } else {
-    navbar.classList.add("navbar-dark");
-    navbar.classList.remove("navbar-light");
-    document
-      .querySelector("meta[name=theme-color]")
-      .setAttribute("content", "#475aff");
+    // sub pages
+    if (
+      window.scrollY > document.getElementById("subpage-slide-1").offsetHeight
+    ) {
+      navbar.classList.add("bgneutral");
+      navbar.classList.remove("bgcolor");
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", "#f1f1f1");
+    } else {
+      navbar.classList.add("bgcolor");
+      navbar.classList.remove("bgneutral");
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", "#bbc2ff");
+    }
   }
 };
